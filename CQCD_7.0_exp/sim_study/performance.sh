39de99b2f5a4320fc024bce80ba11ea9da5922dd
Jian Cao <jian.cao@kaust.edu.sa>
2021-09-23 08:40:16 -0500

    Code for producing performance measurements and latex table

---
+++ CQCD_7.0_exp/sim_study/performance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for nd in 500_100 500_1000 5000_100 5000_1000 25000_100 25000_1000
+do
+    for mtd in bridge_FB_CQCD Fisher_scoring forward_select Yi2011
+    do
+#        echo "Rscript --vanilla performance.R ${mtd}_${nd}.RData data_35000.RData table.csv"
+        Rscript --vanilla performance.R "${mtd}_${nd}.RData" "data_35000.RData" "table.csv"
+        Rscript --vanilla performance.R "${mtd}_dep_${nd}.RData" "data_dep_35000.RData" "table.csv"
+    done
+done 
